108035f50f6dc386e8970dccf93bd10c4bd668b2
Nestor Bolivar <nestor@littlebirdie.com.au>
2022-12-17 13:01:22 +1100

    Updating tests

--- gscrapper.py
+++ gscrapper.py
@@ -2,29 +2,37 @@
 
 import re
 import requests
-import threading
+import json
 from bs4 import BeautifulSoup
 
-buscar = 'ice cream'
+buscar = 'sega genesis'
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
 
-r = requests.get('https://www.google.com/search?tbm=shop&hl=en-GB&psb=1&ved=0CAAQvOkFahcKEwigpLCZsf77AhUAAAAAHQAAAAAQCg&gs_ss=1&oq&gs_lcp=Cgtwcm9kdWN0cy1jYxABGAAyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCUABYAGC9KWgBcAB4AIABAIgBAJIBAJgBALABCg&sclient=products-cc' + '&q=' + buscar.replace(' ', '+'), headers=headers)
+r = requests.get('https://www.google.com/search?tbm=shop&hl=en-GB&psb=1&ved=0CAAQvOkFahcKEwigpLCZsf77AhUAAAAAHQAAAAAQCg&gs_ss=1&oq&gs_lcp=Cgtwcm9kdWN0cy1jYxABGAAyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCMggIABCPARDqAjIICAAQjwEQ6gIyCAgAEI8BEOoCUABYAGC9KWgBcAB4AIABAIgBAJIBAJgBALABCg&sclient=products-cc&q=' + buscar.replace(' ', '+'), headers=headers)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 products = soup.find_all("div", {"class": "sh-dgr__gr-auto sh-dgr__grid-result"})
+# print(soup.prettify())
 
-def process_string(product):
+# for product in (product for product in products):
+for product in products:
+    gpid = product.get("data-docid") # Google Product ID
     title = product.h3.decode_contents()
     price = product.find(string=re.compile("\$"))
-    imagen = product.img
-    print(title, price, imagen)
+    imagen = product.img.get("src")
+    stars = product.find(string=re.compile("out of")) or "No reviews"
+    # Known issue with aliexpress
+    purl = product.find("a", {"class": "shntl"}).get("href").replace('/url?url=', '').split('%3F', 1)[0]
 
-threads = []
-for product in products:
-    thread = threading.Thread(target=process_string, args=(product,))
-    thread.start()
-    # threads.append(thread)
+    # print('####################################')
 
-# Wait for all threads to complete
-#for thread in threads:
-#    thread.join()
+    value = {
+       "gpid": gpid,
+       "product": title,
+       "price": price,
+       "stars": stars,
+       "url": purl,
+       "img": imagen
+     }
+
+    print(json.dumps(value, indent=2))
